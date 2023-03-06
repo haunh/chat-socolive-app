35d99d24a47f53d2af5cdefca307c15b1ed90efc
Hau Ng <haunh.bu@gmail.com>
2023-03-06 14:50:06 +0700

    update code chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("https://api.haunh.com");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("868459");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      setRoom('868459')
+      socket.emit("join_room", room);
+      
+      setShowChat(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showChat ? (
+        <div className="chatContainer">
+          <h3>Pls Input Username</h3>
+          <input
+            type="text"
+            placeholder="Trưởng Thôn"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Socolive Chat</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
   );
 }
